f5061e8c980a65cc467247540eabb027a9cf0857
yoiang <yo.ian.g@gmail.com>
2021-07-28 20:35:08 +0000

    Deploying to gh-pages from @ yoiang/chip-8_rust@c3a17a94da9a161a7128892df29730a54437e226 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -198,7 +198,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/chip8_wasm_bg.wasm":"d81b68469d55f2057afb"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/chip8_wasm_bg.wasm":"d05193903976b427bcec"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
